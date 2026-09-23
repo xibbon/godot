87902128dab253740c82ff3091846faf91918dc0
Miguel de Icaza <miguel@gnome.org>
2026-09-23 17:43:09 -0400

    Fix iOS export MoltenVK references and xcodebuild errors

--- platform/ios/export/export_plugin.cpp
+++ platform/ios/export/export_plugin.cpp
@@ -410,17 +410,25 @@ String EditorExportPlatformIOS::_process_config_file_line(const Ref<EditorExport
 
 		// MoltenVK Framework
 	} else if (p_line.contains("$moltenvk_buildfile")) {
-		String value = "9039D3BE24C093AC0020482C /* MoltenVK.xcframework in Frameworks */ = {isa = PBXBuildFile; fileRef = 9039D3BD24C093AC0020482C /* MoltenVK.xcframework */; };";
-		strnew += p_line.replace("$moltenvk_buildfile", value) + "\n";
+		if (p_config.has_moltenvk) {
+			String value = "9039D3BE24C093AC0020482C /* MoltenVK.xcframework in Frameworks */ = {isa = PBXBuildFile; fileRef = 9039D3BD24C093AC0020482C /* MoltenVK.xcframework */; };";
+			strnew += p_line.replace("$moltenvk_buildfile", value) + "\n";
+		}
 	} else if (p_line.contains("$moltenvk_fileref")) {
-		String value = "9039D3BD24C093AC0020482C /* MoltenVK.xcframework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.xcframework; name = MoltenVK; path = MoltenVK.xcframework; sourceTree = \"<group>\"; };";
-		strnew += p_line.replace("$moltenvk_fileref", value) + "\n";
+		if (p_config.has_moltenvk) {
+			String value = "9039D3BD24C093AC0020482C /* MoltenVK.xcframework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.xcframework; name = MoltenVK; path = MoltenVK.xcframework; sourceTree = \"<group>\"; };";
+			strnew += p_line.replace("$moltenvk_fileref", value) + "\n";
+		}
 	} else if (p_line.contains("$moltenvk_buildphase")) {
-		String value = "9039D3BE24C093AC0020482C /* MoltenVK.xcframework in Frameworks */,";
-		strnew += p_line.replace("$moltenvk_buildphase", value) + "\n";
+		if (p_config.has_moltenvk) {
+			String value = "9039D3BE24C093AC0020482C /* MoltenVK.xcframework in Frameworks */,";
+			strnew += p_line.replace("$moltenvk_buildphase", value) + "\n";
+		}
 	} else if (p_line.contains("$moltenvk_buildgrp")) {
-		String value = "9039D3BD24C093AC0020482C /* MoltenVK.xcframework */,";
-		strnew += p_line.replace("$moltenvk_buildgrp", value) + "\n";
+		if (p_config.has_moltenvk) {
+			String value = "9039D3BD24C093AC0020482C /* MoltenVK.xcframework */,";
+			strnew += p_line.replace("$moltenvk_buildgrp", value) + "\n";
+		}
 
 		// Launch Storyboard
 	} else if (p_line.contains("$plist_launch_screen_name")) {
